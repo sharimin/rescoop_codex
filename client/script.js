81e819cf927933f7e185f836f747aac03d64ab1b
min <sharimin.rashid@gmail.com>
2023-01-04 15:27:34 +0800

    backend

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
 
-    const response = await fetch('https://re-scoop-codex.onrender.com', {
+    const response = await fetch('http://localhost:5000', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
